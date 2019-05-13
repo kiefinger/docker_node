b0bf8ed9529281fe0c974ee68b81e1c2468ec4f2
Josef Kiefinger <josef.kiefinger@gmx.de>
2019-05-13 20:13:17 +0200

    mv to other dirs

---
+++ msg-api-node-3/app/index.js
@@ -0,0 +1,32 @@
+
+
+const http = require ( 'http' );
+
+const server = http.createServer ((req,res) => {
+
+	let body = '';
+	req.setEncoding ('utf8');
+	
+	req.on ( 'data', (chunk) => {
+		body += chunk;
+	});
+
+	req.on ('end' , () => {
+		try {
+			console.log ('got request, again');
+			const data = JSON.parse (body);
+			res.write ( typeof data);
+			res.end();
+		} catch (err) {
+			res.statusCode = 400;
+			return res.end ( `error: ${err.message}` );
+		}
+	});
+});
+
+process.on ( 'SIGTERM', () => {
+console.info('Exiting');
+server.close();
+});
+
+server.listen (1337);
